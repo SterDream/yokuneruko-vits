56b76bc150b6575674595d94f5e4e80abb93e9a4
SterDream <130822110+SterDream@users.noreply.github.com>
2026-01-10 00:49:16 +0900

    Update mel_processing.py

--- F0_MB_iSTFT_VITS/mel_processing.py
+++ F0_MB_iSTFT_VITS/mel_processing.py
@@ -1,19 +1,9 @@
-import math
-import os
-import random
 import torch
-from torch import nn
-import torch.nn.functional as F
-import torch.utils.data
-import numpy as np
 import librosa
-import librosa.util as librosa_util
-from librosa.util import normalize, pad_center, tiny
-from scipy.signal import get_window
-from scipy.io.wavfile import read
-import librosa
+import utils
 
 MAX_WAV_VALUE = 32768.0
+hps = utils.get_hparams()
 
 
 def dynamic_range_compression_torch(x, C=1, clip_val=1e-5):
@@ -63,8 +53,18 @@ def spectrogram_torch(y, n_fft, sampling_rate, hop_size, win_size, center=False)
     y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
     y = y.squeeze(1)
 
-    spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[wnsize_dtype_device],
-                      center=center, pad_mode='reflect', normalized=False, onesided=True, return_complex=False)
+    spec = torch.stft(
+        y,
+        n_fft,
+        hop_length=hop_size,
+        win_length=win_size,
+        window=hann_window[wnsize_dtype_device],
+        center=center,
+        pad_mode='reflect',
+        normalized=False,
+        onesided=True,
+        return_complex=False
+    )
 
     spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
     return spec
@@ -74,6 +74,7 @@ def spec_to_mel_torch(spec, n_fft, num_mels, sampling_rate, fmin, fmax):
     global mel_basis
     dtype_device = str(spec.dtype) + '_' + str(spec.device)
     fmax_dtype_device = str(fmax) + '_' + dtype_device
+
     if fmax_dtype_device not in mel_basis:
         mel = librosa.filters.mel(
             sr=sampling_rate,
@@ -83,6 +84,7 @@ def spec_to_mel_torch(spec, n_fft, num_mels, sampling_rate, fmin, fmax):
             fmax=fmax
         )
         mel_basis[fmax_dtype_device] = torch.from_numpy(mel).to(dtype=spec.dtype, device=spec.device)
+
     spec = torch.matmul(mel_basis[fmax_dtype_device], spec)
     spec = spectral_normalize_torch(spec)
     return spec
@@ -120,5 +122,4 @@ def mel_spectrogram_torch(y, n_fft, num_mels, sampling_rate, hop_size, win_size,
 
     spec = torch.matmul(mel_basis[fmax_dtype_device], spec)
     spec = spectral_normalize_torch(spec)
-
     return spec
